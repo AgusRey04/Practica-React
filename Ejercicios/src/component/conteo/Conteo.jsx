6a1d904dc8c19693fd92e8539acaee644ab3422c
AgusRey04 <reymundez37@gmail.com>
2024-03-30 01:57:00 -0300

    Ejercicio 3

--- Ejercicios/src/component/conteo/Conteo.jsx
+++ Ejercicios/src/component/conteo/Conteo.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
 const Conteo = ({ beers }) => {
-    const flitrado = beers.filter((arre)=> {
-        if (arre.beerStyle=="Red"|| arre.beerStyle=="IPA"){
-            return {arre}
+    const flitrado = beers.filter((beer)=> {
+        if (beer.beerStyle=="Red"|| beer.beerStyle=="IPA"){
+            return {beer}
         }
     });
     
   
     return (
       <div>
-        <h5>Cantidad de cervezas de estilo Red e IPA:</h5>
+        <h5>Cantidad de cervezas de estilo Red e IPA</h5>
         <p>{flitrado.length}</p>
       </div>
     );
